a97a68debb3a2a0815daa6f6974f4f9eb61720f9
tomsarry <55455075+tomsarry@users.noreply.github.com>
2020-08-31 17:38:54 -0400

    added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export interface FooterProps {}
+
+const Footer: React.SFC<FooterProps> = () => {
+  return <div className="footer">FOOTER</div>;
+};
+
+export default Footer;
